2613c023582649e96c59228d0edf7c1844815654
adriiana-dev <adrianateste52@gmail.com>
2026-03-20 13:33:36 -0300

    Atualizando e refazendo funcoes

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -1,35 +1,30 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-let config;
+const conexao = new Pool(
+  process.env.PGHOST
+    ? {
+        host:     process.env.PGHOST,
+        port:     process.env.PGPORT,
+        database: process.env.PGDATABASE,
+        user:     process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        ssl:      { rejectUnauthorized: false }
+      }
+    : {
+        host:     process.env.DB_HOST,
+        port:     process.env.DB_PORTA,
+        database: process.env.DB_NOME,
+        user:     process.env.DB_USUARIO,
+        password: process.env.DB_SENHA,
+      }
+);
 
-if (process.env.DATABASE_URL) {
-  config = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false 
-    }
-  };
-} else {
-  
-  config = {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORTA || 5432,
-    database: process.env.DB_NOME,
-    user: process.env.DB_USUARIO,
-    password: process.env.DB_SENHA,
-    ssl: false 
-  };
-}
-
-const conexao = new Pool(config);
-
-conexao.query("SELECT 1")
-  .then(() => {
-    console.log("✅ Conectado ao PostgreSQL com sucesso!");
-  })
-  .catch((erro) => {
-    console.error("❌ Erro ao conectar no banco de dados:", erro.message);
-  });
+conexao.query("SELECT 1").then(() => {
+  console.log("Conectado ao PostgreSQL com sucesso!");
+}).catch((erro) => {
+  console.error("Erro ao conectar no banco de dados:", erro.message);
+  process.exit(1);
+});
 
 module.exports = conexao;
